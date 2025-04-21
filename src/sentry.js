89c6c73d89603d85e59398d93ecd826f50b656a2
KingHarry001 <kingharrisonariwodo@gmail.com>
2025-04-21 22:13:44 +0100

    Setup Sentry error monitoring

---
+++ src/sentry.js
@@ -0,0 +1,9 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", "zentry-eosin.vercel.app"],
+});
