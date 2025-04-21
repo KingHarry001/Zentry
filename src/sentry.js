92176f31e90bcf31967a0754feb097fb56e599c3
KingHarry001 <kingharrisonariwodo@gmail.com>
2025-04-21 22:43:19 +0100

    removed old deprecated

--- src/sentry.js
+++ src/sentry.js
@@ -1,8 +1,9 @@
+// sentry.js or sentry.ts
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/browser"; // ✅ from @sentry/browser
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dsn: "https://f4d32f206498fd7d4e9922ac308a9755@o4509175748886528.ingest.us.sentry.io/4509193212264448",
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", "zentry-eosin.vercel.app"],
