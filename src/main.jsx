bb64174c6e68d2c8212d3f40daea5f3ed428a32d
KingHarry001 <kingharrisonariwodo@gmail.com>
2025-04-21 22:19:12 +0100

    Setup Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://f4d32f206498fd7d4e9922ac308a9755@o4509175748886528.ingest.us.sentry.io/4509193212264448",
-  integrations: [Sentry.browserTracingIntegration()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", "zentry-eosin.vercel.app"],
-});
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
