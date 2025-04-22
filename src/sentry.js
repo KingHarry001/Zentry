4121cdaab126ea5060ecde8961158eab1467f063
KingHarry001 <kingharrisonariwodo@gmail.com>
2025-04-22 08:56:28 +0100

    ran build

--- src/sentry.js
+++ src/sentry.js
@@ -1,10 +1,11 @@
-// sentry.js or sentry.ts
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/browser"; // ✅ from @sentry/browser
 
 Sentry.init({
   dsn: "https://f4d32f206498fd7d4e9922ac308a9755@o4509175748886528.ingest.us.sentry.io/4509193212264448",
-  integrations: [new BrowserTracing()],
+  integrations: [
+    Sentry.browserTracingIntegration({
+      tracePropagationTargets: ["localhost", "zentry-eosin.vercel.app"],
+    }),
+  ],
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", "zentry-eosin.vercel.app"],
 });
